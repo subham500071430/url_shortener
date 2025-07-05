b96ecac9597a1e173ca3f06ea6de7b2b15e9d246
subham <subham10022000@gmail.com>
2025-07-05 21:10:32 +0530

    added transactional for atomicity

--- src/main/java/com/app/URLShortener/service/UrlShortenerServiceImpl.java
+++ src/main/java/com/app/URLShortener/service/UrlShortenerServiceImpl.java
@@ -7,6 +7,7 @@ import com.app.URLShortener.repository.UrlRepository;
 import com.app.URLShortener.util.RedisLookup;
 import com.app.URLShortener.util.RedisReverseLookup;
 import com.app.URLShortener.util.URLGenerator;
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,53 +26,42 @@ public class UrlShortenerServiceImpl implements UrlShortenerService {
     private URLGenerator generator;
     @Autowired(required = false)
     private RedisLookup redisLookup;
-    @Autowired(required = false)
+    @Autowired
     private RedisReverseLookup redisReverseLookup;
-    @Autowired(required = false)
+    @Autowired
     private UrlRepository urlRepository;
 
 
     @Override
+    @Transactional
     public UrlShortenResponse shortenURL(UrlShortenRequest request) {
 
         String generatedShortUrl = null;
         String longUrl = request.getLongUrl();
         String prefixUrl = "http://localhost:8080/";
 
-        if (urlRepository != null) {
+        // check cache
+        if (redisReverseLookup.isLongUrlUsed(longUrl))
+            return new UrlShortenResponse(redisReverseLookup.getShortCode(longUrl));
 
-            Optional<UrlMapper> urlMapper = urlRepository.findByLongUrl(longUrl);
+        // check db
+        Optional<UrlMapper> urlMapper = urlRepository.findByLongUrl(longUrl);
 
-            if(urlMapper.isPresent()){
-                  return new UrlShortenResponse(urlMapper.get().getShortUrl());
-            } else {
-                do {
-                    generatedShortUrl = prefixUrl + generator.generate(longUrl);
-                } while (urlRepository.findById(generatedShortUrl).isPresent());
+        if (urlMapper.isPresent())
+            return new UrlShortenResponse(urlMapper.get().getShortUrl());
 
-                urlRepository.save(new UrlMapper(generatedShortUrl,longUrl));
-                return  new UrlShortenResponse(generatedShortUrl);
-            }
+        // generate short url
+        do {
+            generatedShortUrl = prefixUrl + generator.generate(longUrl);
+        } while (urlRepository.findById(generatedShortUrl).isPresent());
 
-        } else {
+        urlRepository.save(new UrlMapper(generatedShortUrl, longUrl));
+        redisLookup.cacheShortUrl(generatedShortUrl, longUrl);
+        redisLookup.markCodeAsUsed(generatedShortUrl);
+        redisReverseLookup.cacheLongUrl(longUrl, generatedShortUrl);
+        redisReverseLookup.markLongUrlAsUsed(longUrl);
 
-            if (redisReverseLookup.isLongUrlUsed(longUrl)) {
-                return new UrlShortenResponse(redisReverseLookup.getShortCode(longUrl));
-            }
-
-            do {
-                generatedShortUrl = prefixUrl + generator.generate(longUrl);
-            } while (redisLookup.isCodeUsed(generatedShortUrl));
-
-
-            redisLookup.cacheShortUrl(generatedShortUrl, longUrl);
-            redisLookup.markCodeAsUsed(generatedShortUrl);
-            redisReverseLookup.cacheLongUrl(longUrl, generatedShortUrl);
-            redisReverseLookup.markLongUrlAsUsed(longUrl);
-
-            return new UrlShortenResponse(generatedShortUrl);
-
-        }
+        return new UrlShortenResponse(generatedShortUrl);
 
     }
 
@@ -81,10 +71,10 @@ public class UrlShortenerServiceImpl implements UrlShortenerService {
         String fullShortUrl = "http://localhost:8080/" + shortUrl;
         String longUrl = null;
 
-        if(Objects.nonNull(redisLookup) && Objects.nonNull(redisLookup.getLongUrl(fullShortUrl))){
-             longUrl = redisLookup.getLongUrl(fullShortUrl);
-        } else if(Objects.nonNull(urlRepository) && urlRepository.findById(fullShortUrl).isPresent()){
-             longUrl =  urlRepository.findById(fullShortUrl).get().getLongUrl();
+        if (Objects.nonNull(redisLookup) && Objects.nonNull(redisLookup.getLongUrl(fullShortUrl))) {
+            longUrl = redisLookup.getLongUrl(fullShortUrl);
+        } else if (Objects.nonNull(urlRepository) && urlRepository.findById(fullShortUrl).isPresent()) {
+            longUrl = urlRepository.findById(fullShortUrl).get().getLongUrl();
         }
 
         return new UrlShortenResponse(longUrl);
